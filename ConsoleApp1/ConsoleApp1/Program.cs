6ef4cdf688767542a57ea12a35c71abff294d864
rixcypz <chaiyapruekworasiha@CHAIYAPRUEK-SRS.local>
2025-02-17 12:05:23 +0700

    [initial] - create console app.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(password);
